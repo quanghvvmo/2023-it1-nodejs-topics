8b7695bf6df733df78c7063bd98a29eb2050a117
PeLaiahT <87683500+PeLaiahT@users.noreply.github.com>
2023-03-30 16:39:05 +0700

    Swagger js
    Check

--- Excercise/Day4/src/server.js
+++ Excercise/Day4/src/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import initWebroutes from "./route/web";
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs'
 import connectDB from "./config/database"
 //import connectDB from "./config/connectMongo"
 require('dotenv').config();
-
+const swaggerJsDocs = YAML.load('api.yaml')
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
 
 connectDB();
 initWebroutes(app);
